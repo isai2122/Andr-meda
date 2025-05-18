b4dd784e4cc8214e8aef55810cdff9b6bca28773
isai2122 <ortizsacc18@gmail.com>
2025-05-18 16:41:23 -0500

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -1128,6 +1128,16 @@ const productos = [
     imagen: "/images/Images/125.jpg",
     categoria: "Higiene"
   },
+  {
+  "id": 126,
+  "nombre": "Reloj de buceo San Martin 38mm NH35 automático",
+  "tienda": "AliExpress",
+  "precio": "186.000 COP",
+  "enlace": "https://s.click.aliexpress.com/e/_olMlqDl",
+  "imagen": "/images/Images/Sb7a019cdaf4e400cb1a0f70e14852b68N.jpg_960x960q75.jpg_.avif",
+  "categoria": "A la moda"
+},
+
 ];
 ];
 
